3be7b48fb938e7e6a9ed5fbf92e08c64782d6588
Daniela <dggtn9@gmail.com>
2024-02-21 20:17:29 -0300

    my first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded' ,() =>{
+    const scoreDisplay = document.getElementById('score')
+    const width = 28
+    let score = 0
+    document.querySelector('button').addEventListener('click',()=>{})
+})
