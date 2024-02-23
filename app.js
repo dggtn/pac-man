3ae4ada57446eb426f416d2611b7433df9971155
Daniela <dggtn9@gmail.com>
2024-02-23 14:12:21 -0300

    estilos

--- app.js
+++ app.js
@@ -35,15 +35,10 @@ document.addEventListener("DOMContentLoaded", () => {
         1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
     ]
 
-    // 0 - pac-dots
-    // 1 - wall
-    // 2 - ghost-lair
-    // 3 - power-pellet
-    // 4 - empty
 
     const squares = []
 
-    //create your board
+
     function createBoard() {
         for (let i = 0; i < layout.length; i++) {
             const square = document.createElement("div")
@@ -51,7 +46,7 @@ document.addEventListener("DOMContentLoaded", () => {
             grid.appendChild(square)
             squares.push(square)
 
-            //add layout to the board
+
             if (layout[i] === 0) {
                 squares[i].classList.add("pac-dot")
             }
@@ -68,17 +63,14 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     createBoard()
 
-    //create Characters
-    // draw pac-man onto the board
     let pacmanCurrentIndex = 490
     squares[pacmanCurrentIndex].classList.add("pac-man")
 
-    //move pacman
     function movePacman(e) {
         squares[pacmanCurrentIndex].classList.remove("pac-man")
-        // switch (e.keyCode) { deprecated
+
         switch (e.key) {
-            // case 37:
+
             case "ArrowLeft":
                 if (
                     pacmanCurrentIndex % width !== 0 &&
@@ -92,7 +84,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
                 break
             case "ArrowUp":
-                // case 38:
+
                 if (
                     pacmanCurrentIndex - width >= 0 &&
                     !squares[pacmanCurrentIndex - width].classList.contains("wall") &&
@@ -103,7 +95,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
                 break
             case "ArrowRight":
-                // case 39:
+
                 if (
                     pacmanCurrentIndex % width < width - 1 &&
                     !squares[pacmanCurrentIndex + 1].classList.contains("wall") &&
@@ -118,7 +110,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 }
                 break
             case "ArrowDown":
-                // case 40:
+
                 if (
                     pacmanCurrentIndex + width < width * width &&
                     !squares[pacmanCurrentIndex + width].classList.contains("wall") &&
@@ -132,12 +124,12 @@ document.addEventListener("DOMContentLoaded", () => {
         pacDotEaten()
         powerPelletEaten()
         checkForGameOver()
-        checkForWin()
+        VerSiGana()
     }
 
     document.addEventListener("keyup", movePacman)
 
-    //what happens when you eat a pac-dot
+
     function pacDotEaten() {
         if (squares[pacmanCurrentIndex].classList.contains("pac-dot")) {
             score++
@@ -146,7 +138,6 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    //what happens when you eat a power-pellet
     function powerPelletEaten() {
         if (squares[pacmanCurrentIndex].classList.contains("power-pellet")) {
             score += 10
@@ -157,12 +148,12 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    //make the ghosts stop flashing
+
     function unScareGhosts() {
         ghosts.forEach(ghost => ghost.isScared = false)
     }
 
-    //create ghosts using Constructor
+
     class Ghost {
         constructor(className, startIndex, speed) {
             this.className = className
@@ -175,7 +166,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    //all my ghosts
+
     const ghosts = [
         new Ghost("blinky", 348, 250),
         new Ghost("pinky", 376, 400),
@@ -183,11 +174,11 @@ document.addEventListener("DOMContentLoaded", () => {
         new Ghost("clyde", 379, 500),
     ]
 
-    //draw my ghosts onto the grid
+
     ghosts.forEach(ghost =>
         squares[ghost.currentIndex].classList.add(ghost.className, "ghost"))
 
-    //move ghosts randomly
+
     ghosts.forEach(ghost => moveGhost(ghost))
 
     function moveGhost(ghost) {
@@ -195,7 +186,7 @@ document.addEventListener("DOMContentLoaded", () => {
         let direction = directions[Math.floor(Math.random() * directions.length)]
 
         ghost.timerId = setInterval(function () {
-            //if next square your ghost is going to go to does not have a ghost and does not have a wall
+
             if (
                 !squares[ghost.currentIndex + direction].classList.contains("ghost") &&
                 !squares[ghost.currentIndex + direction].classList.contains("wall")
@@ -203,14 +194,13 @@ document.addEventListener("DOMContentLoaded", () => {
                 squares[ghost.currentIndex].classList.remove(ghost.className, "ghost", "scared-ghost")
                 ghost.currentIndex += direction
                 squares[ghost.currentIndex].classList.add(ghost.className, "ghost")
-                // else find a new random direction to go in
+
             } else direction = directions[Math.floor(Math.random() * directions.length)]
-            // if the ghost is currently scared
+
             if (ghost.isScared) {
                 squares[ghost.currentIndex].classList.add("scared-ghost")
             }
 
-            //if the ghost is currently scared and pacman is on it
             if (ghost.isScared && squares[ghost.currentIndex].classList.contains("pac-man")) {
                 ghost.isScared = false
                 squares[ghost.currentIndex].classList.remove(ghost.className, "ghost", "scared-ghost")
@@ -223,7 +213,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }, ghost.speed)
     }
 
-    //check for a game over
+
     function checkForGameOver() {
         if (
             squares[pacmanCurrentIndex].classList.contains("ghost") &&
@@ -236,8 +226,8 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
-    
-    function checkForWin() {
+
+    function VerSiGana() {
         if (score >= 274) {
             ghosts.forEach(ghost => clearInterval(ghost.timerId))
             document.removeEventListener("keyup", movePacman)
